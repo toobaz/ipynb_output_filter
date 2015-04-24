8884f5e5b6c293ed2ac3025d358fd565ddac8130
Pietro Battiston <me@pietrobattiston.it>
2015-04-24 08:53:00 +0200

    (Probably unnecessarily complex) support for jupyter

--- ipynb_output_filter.py
+++ ipynb_output_filter.py
@@ -1,19 +1,46 @@
 #! /usr/bin/python
 
 import sys
-from IPython.nbformat.current import read, write
 
-json_in = read(sys.stdin, 'json')
+version = None
 
-for sheet in json_in.worksheets:
+try:
+    # Jupyter
+    from jupyter_nbformat import reads, write
+except ImportError:
+    try:
+        # New IPython
+        from IPython.nbformat import reads, write
+    except ImportError:
+        # Old IPython
+        from IPython.nbformat.current import reads, write
+        version = 'json'
+
+to_parse = sys.stdin.read()
+
+if not version:
+    import json
+    json_in = json.loads(to_parse)
+    version = json_in['nbformat']
+
+json_in = reads(to_parse, version)
+
+if hasattr(json_in, 'worksheets'):
+    # IPython
+    sheets = json_in.worksheets
+else:
+    # Jupyter
+    sheets = [json_in]
+
+for sheet in sheets:
     for cell in sheet.cells:
         if "outputs" in cell:
             cell.outputs = []
         for field in ("prompt_number", "execution_number"):
             if field in cell:
-                del cell["prompt_number"]
+                del cell[field]
 
 if 'signature' in json_in.metadata:
     json_in.metadata['signature'] = ""
 
-write(json_in, sys.stdout, 'json')
+write(json_in, sys.stdout, version)
