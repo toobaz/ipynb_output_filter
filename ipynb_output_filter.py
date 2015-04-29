68f6bd0a5a40ffee513e76586f55cc61f5f2677f
Pietro Battiston <me@pietrobattiston.it>
2015-04-29 15:18:28 +0200

    Also remove "execution_count" (which cannot just be deleted)

--- ipynb_output_filter.py
+++ ipynb_output_filter.py
@@ -39,6 +39,9 @@ for sheet in sheets:
         for field in ("prompt_number", "execution_number"):
             if field in cell:
                 del cell[field]
+        for field in ("execution_count",):
+            if field in cell:
+                cell[field] = "null"
 
 if 'signature' in json_in.metadata:
     json_in.metadata['signature'] = ""
